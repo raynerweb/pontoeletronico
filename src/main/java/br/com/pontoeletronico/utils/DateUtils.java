5b474bbf43d9858ae32f4b0516560c4782748a1f
raynerweb <raynerweb@gmail.com>
2015-09-15 09:36:26 -0300

    DateUtils

---
+++ src/main/java/br/com/pontoeletronico/utils/DateUtils.java
@@ -0,0 +1,42 @@
+package br.com.pontoeletronico.utils;
+
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.ZoneId;
+import java.util.Date;
+
+public class DateUtils {
+
+	public static LocalDate toLocalDate(Date date) {
+		return date.toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
+	}
+
+	public static LocalTime toLocalTime(Date date) {
+		return toLocalDateTime(date).toLocalTime();
+	}
+
+	public static LocalDateTime toLocalDateTime(Date date) {
+		Instant instant = Instant.ofEpochMilli(date.getTime());
+		return LocalDateTime.ofInstant(instant, ZoneId.systemDefault());
+	}
+
+	public static Date toDate(LocalDateTime localDateTime) {
+		Instant instant = localDateTime.atZone(ZoneId.systemDefault()).toInstant();
+		return Date.from(instant);
+	}
+
+	public static Date toDate(LocalDate localDate) {
+		Instant instant = localDate.atStartOfDay().atZone(ZoneId.systemDefault()).toInstant();
+		return Date.from(instant);
+	}
+
+	public static Date toDate(LocalTime localTime) {
+		LocalDate localDate = LocalDate.now();
+		LocalDate date = LocalDate.of(localDate.getYear(), localDate.getMonth(), localDate.getDayOfMonth());
+		Instant instant = localTime.atDate(date).atZone(ZoneId.systemDefault()).toInstant();
+		return Date.from(instant);
+	}
+
+}
