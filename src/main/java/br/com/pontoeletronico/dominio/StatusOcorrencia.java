17fa8a32f21eb3341f3f05dbefb9b443d0c29869
raynerweb <raynerweb@gmail.com>
2015-09-28 13:23:13 -0300

    status ocorrenia como um como pojo

--- src/main/java/br/com/pontoeletronico/dominio/StatusOcorrencia.java
+++ src/main/java/br/com/pontoeletronico/dominio/StatusOcorrencia.java
@@ -1,19 +1,28 @@
 package br.com.pontoeletronico.dominio;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+@JsonFormat(shape = JsonFormat.Shape.OBJECT)
 public enum StatusOcorrencia {
 
-	ABERTO("AB"), ACEITO("AC"), REJEITADO("RJ");
+	ABERTO("AB", "Aberta"), ACEITO("AC", "Aceita"), REJEITADO("RJ", "Rejeitada");
 	private String sigla;
+	private String descricao;
 
-	private StatusOcorrencia(String sigla) {
+	private StatusOcorrencia(String sigla, String descricao) {
 		this.sigla = sigla;
+		this.descricao = descricao;
 	}
 
 	public String getSigla() {
 		return this.sigla;
 	}
 
-	public static StatusOcorrencia getTipoStatus(String sigla) {
+	public String getDescricao() {
+		return this.descricao;
+	}
+
+	public static StatusOcorrencia getStatusOcorrenciaSigla(String sigla) {
 		for (StatusOcorrencia status : StatusOcorrencia.values()) {
 			if (status.getSigla().equals(sigla))
 				return status;
