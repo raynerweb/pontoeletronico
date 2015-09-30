37b9c69e8856b1326e0ad8fb7c791c9b9d55243a
raynerweb <raynerweb@gmail.com>
2015-09-30 14:54:58 -0300

    add Bean Validation

--- src/main/java/br/com/pontoeletronico/JerseyConfig.java
+++ src/main/java/br/com/pontoeletronico/JerseyConfig.java
@@ -3,12 +3,19 @@ package br.com.pontoeletronico;
 import javax.ws.rs.ApplicationPath;
 
 import org.glassfish.jersey.server.ResourceConfig;
+import org.glassfish.jersey.server.ServerProperties;
 
 @ApplicationPath("/rest")
 public class JerseyConfig extends ResourceConfig {
 
 	public JerseyConfig() {
+
+		// add Bean Validation
+		property(ServerProperties.BV_SEND_ERROR_IN_RESPONSE, true);
+		property(ServerProperties.BV_DISABLE_VALIDATE_ON_EXECUTABLE_OVERRIDE_CHECK, true);
+
 		packages("br.com.pontoeletronico.rest");
 	}
-	
+
 }
+
